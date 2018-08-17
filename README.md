8f2d7294f375b24f9e3b3a3f57a0cad70d9397e9
Simon Taylor <simon@elementdev.co.uk>
2018-08-17 17:42:01 +0100

    Update examples functions on service and readme.

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
+
 #  Inline Manual Angular Integration
 
-This is a simple example of how [Inline Manual](https://inlinemanual.com) may be integrated within an Angular application. It doesn't serve as a complete solution but simply demonstrates how useful it would be to have interfaces available for the inline object on `Window`. 
+  
 
-Such as having IntelliSense available. 
+This is a simple example of how [Inline Manual](https://inlinemanual.com) may be integrated within an Angular application. It doesn't serve as a complete solution but simply demonstrates how useful it would be to have interfaces available for the inline object on `Window`.
+
+  
+
+Such as having IntelliSense available.
+
+  
 
 ![VS Code Example](./example.png)
+
+**Usage**
+
+    import { Component, AfterViewInit }  from  '@angular/core';
+    import { InlineService }  from  'services/inline-manual.service';
+    constructor (private inlineService: InlineService) {}
+    
+    ngAfterViewInit()  {
+	 this.inlineService.showPanel();
+    }
+or
+
+    this.trackingData = {
+      uid: this.User.id,
+      email: this.User.email,
+      username: this.User.username
+      };
+    this.inlineService.createPlayer(this.trackingData);
+
+**Notes**
+Example 1 is called in `ngAFterInit` to ensure that the `<script>` has been loaded from the remote source. Calling functions on `_window` before this will give undefined errors. 
+
+Example 2 Shows you you would call the service with an object to start tracking. 
--- inline-service.ts
+++ inline-service.ts
@@ -1,24 +1,55 @@
-import { Injectable } from '@angular/core';
-import { WindowRefService } from './window-ref.service';
-import { InlineManualTracking } from './interfaces';
+import { Injectable } from "@angular/core";
+import { WindowRefService } from "./window-ref.service";
+import { InlineManualTracking } from "./interfaces";
 
 @Injectable()
-
 export class InlineService {
+  _window: any;
 
-    _window: any;
+  constructor(windowRef: WindowRefService) {
+    this._window = windowRef.nativeWindow;
+  }
 
-    constructor(windowRef: WindowRefService) {
-        this._window = windowRef.nativeWindow;
-    }
+  createPlayer(trackingData: InlineManualTracking) {
+    this._window.createInlineManualPlayer(trackingData);
+  }
 
-    createPlayer(trackingData: InlineManualTracking) {
-        this._window.createInlineManualPlayer(trackingData)
-    }
+  updatePlayer() {
+    this._window.inline_manual_player.update();
+  }
 
-    updatePlayer() {
-        this._window.inline_manual_player.update();
-    }
+  activateTopic(id: string) {
+    this._window.inline_manual_player.activateTopic(id);
+  }
 
-    // furhter methods on this service...
+  showPanel() {
+    this._window.inline_manual_player.showPanel();
+  }
+
+  // incomplete examples
+  // goToStep() {
+  //   this._window.inline_manual_player.goToStep();
+  // }
+  // deactivate() {
+  //   this._window.inline_manual_player.deactivate();
+  // }
+  // getCurrentTopic() {
+  //   this._window.inline_manual_player.getCurrentTopic();
+  // }
+  // getCurrentStep() {
+  //   this._window.inline_manual_player.getCurrentStep();
+  // }
+  // setMetadata() {
+  //   this._window.inline_manual_player.setMetadata();
+  // }
+  // hidePanel() {
+  //   this._window.inline_manual_player.hidePanel();
+  // }
+  // togglePanel() {
+  //   this._window.inline_manual_player.togglePanel();
+  // }
+  // destroy() {
+  //   this._window.inline_manual_player.destroy();
+  // }
+
 }
